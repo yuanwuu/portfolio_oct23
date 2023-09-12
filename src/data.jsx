b30220f3071712346e3a5ea53aa351e049896f4c
Yuan Wu <wu.yuanf@gmail.com>
2023-09-11 21:01:36 -0400

    adding data.jsx

---
+++ src/data.jsx
@@ -0,0 +1,26 @@
+import bookshelf from './assets/bookshelf.png'
+import farkled from './assets/farkled.png'
+import nikeLanding from './assets/nike-landing.png'
+
+const data = [
+    {img: bookshelf,
+    title: 'Bookshelf',
+    link:'https://whimsical-naiad-8feeab.netlify.app'},
+    {img: farkled,
+        title: 'Farkled',
+        link: 'https://www.example.com/farkled'
+    },
+    {img: nikeLanding,
+        title: 'Cloned Nike',
+        link: 'https://www.example.com/farkled'
+    }
+]
+
+// const uxData = [
+//     {img: ,
+//     title: '',
+//     link: '',
+//     desc: ''}
+// ]
+
+export default data
