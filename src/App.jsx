43cb53a1ed7889c2878b9ed94c73c07db0f1fe7f
Yuan Wu <wu.yuanf@gmail.com>
2023-09-16 15:26:43 -0400

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
--- src/data.jsx
+++ src/data.jsx
@@ -7,12 +7,12 @@ const data = [
     title: 'Bookshelf',
     link:'https://whimsical-naiad-8feeab.netlify.app'},
     {img: farkled,
-        title: 'Farkled',
-        link: 'https://www.example.com/farkled'
+    title: 'Farkled',
+    link: 'https://www.example.com/farkled'
     },
     {img: nikeLanding,
-        title: 'Cloned Nike',
-        link: 'https://www.example.com/farkled'
+    title: 'Cloned Nike',
+    link: 'https://www.example.com/farkled'
     }
 ]
 
