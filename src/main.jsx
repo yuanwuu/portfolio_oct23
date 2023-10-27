959be15fc4dadbb13ee00c2911e6346efa11e3f5
Yuan Wu <wu.yuanf@gmail.com>
2023-10-27 13:38:49 -0400

    add: main.jsx/middleware

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import App from './App'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+app.use(express.static('public', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.jsx')) {
+      res.type('text/javascript');
+    }
+  }
+}));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
