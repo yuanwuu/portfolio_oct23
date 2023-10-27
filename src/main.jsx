beaf6c22050e17c5afe7f868b998910103360d71
Yuan Wu <wu.yuanf@gmail.com>
2023-10-27 14:27:52 -0400

    fix:deploy workflow

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,6 @@ import App from './App'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-app.use(express.static('public', {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.jsx')) {
-      res.type('text/javascript');
-    }
-  }
-}));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
